702783595b323212aad58c73f17cae9402741131
EneyeQA <eneye38@gmail.com>
2024-11-21 05:29:13 -0600

    first commit

---
+++ datatype.js/datatypetask2.js
@@ -0,0 +1,5 @@
+const Day = "Thursday"
+const Position = 4
+let Output = 'Today is ' + Day + ', ' + ' It is the ' + Position + 'th day of the week.'
+
+console.log(Output)
---
+++ datatype.js/primitive.js
@@ -0,0 +1,65 @@
+// const myage = 50
+// const age = "fifty"
+
+// console.log(typeof(age))
+// console.log(typeof(myage))
+
+// console.log(myage)
+// console.log(age)
+
+// let numberOfstudents = 190
+// let numberofboys = 90
+// let numberofgirls = 30
+
+
+// console.log(numberOfstudents + 60)
+// console.log(numberofboys / numberofgirls)
+// console.log(numberofboys * numberofgirls)
+// console.log(numberofgirls + 80 / 2)
+
+// const students = 'My school has a total number of ' + numberOfstudents + "," + ' number of girls is ' + numberofgirls + ',' + ' number of boys is ' + numberofboys + '.'
+
+// console.log(students)
+
+// let aboy = true
+// let aGirl = false
+
+// console.log(typeof(aboy))
+// console.log(typeof(aGirl))
+
+// let Result = 130 + 50
+// const result = 150 - 50
+
+// console.log(result)
+// console.log(Result)
+
+// const Output = 2000 / 2
+// const output = 500 % 200
+
+// console.log(Output)
+// console.log(output)
+// console.log(Result * Output)
+
+// let output = 10 > 5
+// let suleimanAge = 50
+// const SamAge = 30
+// let Results = (suleimanAge > SamAge)
+// let result = (SamAge > suleimanAge)
+
+
+// console.log(output)
+// console.log(suleimanAge)
+// console.log(Results)
+// console.log(result)
+
+// let sheriage = 99
+// let alfage = "99"
+// let Age = (sheriage == alfage)
+
+// console.log(Age)
+
+// let age = 90
+// let name = "David"
+// let Hieght = "29kg"
+
+// console.log(age == 90 || name === "David" && Hieght === "29kg")
